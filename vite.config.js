f1e8351fe251b2a8eee8103e83657ebef055ae84
alexvirtech <alex@vir-tec.net>
2024-08-02 19:36:30 +0300

    Fri 08-02-24

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // default port, change if needed
+  },
 })
